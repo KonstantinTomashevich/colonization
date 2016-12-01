47ce6a398dddcfb1aa45e46e52eee9a05ea9c8d8
Konstant <ekit.mail@gmail.com>
2016-12-01 19:55:53 +0200

    Update arrays bindings with Urho3D templates.

--- sources/Colonization/AngelScriptBinders/BindingMacroses.hpp
+++ sources/Colonization/AngelScriptBinders/BindingMacroses.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <Colonization/BuildConfiguration.hpp>
+#include <Urho3D/AngelScript/APITemplates.h>
+
 #ifdef NDEBUG
     #define CHECK_ANGELSCRIPT_RETURN(expression) expression
 #else
@@ -56,49 +58,29 @@
     BIND_OBJECT_PROPERTY_GETTER (asEngine, cxxObjectType, asObjectType, asPropertyType, propertyName); \
     BIND_OBJECT_PROPERTY_SETTER (asEngine, cxxObjectType, asObjectType, asPropertyType, propertyName)
 
-#define OBJECT_ARRAY_PROPERTY_ACESSOR(objectType, arrayValuesType, propertyName) \
-    arrayValuesType objectType ## _get_value_of_ ## propertyName ## _at_index (objectType * object, int index) \
+#define OBJECT_ARRAY_PROPERTY_ACESSOR(objectType, arrayType, arrayValuesType, propertyName, asArrayDecl) \
+    Urho3D::CScriptArray *objectType ## _get_ ## propertyName (objectType * object) \
     { \
-        assert (index < object->propertyName.Size ()); \
-        return object->propertyName.At (index); \
+        return Urho3D::VectorToArray <arrayValuesType> (object->propertyName, asArrayDecl); \
     } \
     \
-    int objectType ## _get_size_of_ ## propertyName (objectType * object) \
+    void objectType ## _set_ ## propertyName (objectType * object, Urho3D::CScriptArray *array) \
     { \
-        return object->propertyName.Size (); \
+        object->propertyName = Urho3D::ArrayTo ## arrayType <arrayValuesType> (array); \
     } \
-    \
-    int objectType ## _set_value_of_ ## propertyName ## _at_index (objectType *object, int index, arrayValuesType value) \
-    { \
-        if (index < object->propertyName.Size ()) \
-        { \
-            object->propertyName [index] = value; \
-            return index; \
-        } \
-        else \
-        { \
-            object->propertyName.Push (value); \
-            return (object->propertyName.Size () - 1); \
-        } \
-    }
 
 #define BIND_OBJECT_ARRAY_PROPERTY(asEngine, cxxObjectType, asObjectType, asArrayValuesType, propertyName) \
     CHECK_ANGELSCRIPT_RETURN ( \
-                asEngine->RegisterObjectMethod (#asObjectType, (#asArrayValuesType + Urho3D::String (" get_array_") \
-                + #propertyName + Urho3D::String ("_value (int index)")).CString (), \
-                asFUNCTION (cxxObjectType ## _get_value_of_ ## propertyName ## _at_index), \
+                asEngine->RegisterObjectMethod (#asObjectType, (Urho3D::String ("Array <") \
+                + #asArrayValuesType + Urho3D::String (">@") + Urho3D::String (" get_") \
+                + #propertyName + Urho3D::String (" ()")).CString (), \
+                asFUNCTION (cxxObjectType ## _get_ ## propertyName), \
                 asCALL_CDECL_OBJFIRST) \
                 ); \
     CHECK_ANGELSCRIPT_RETURN ( \
-                asEngine->RegisterObjectMethod (#asObjectType, (Urho3D::String ("int get_array_") \
-                + #propertyName + Urho3D::String ("_size ()")).CString (), \
-                asFUNCTION (cxxObjectType ## _get_size_of_ ## propertyName), \
+                asEngine->RegisterObjectMethod (#asObjectType, (Urho3D::String ("void set_") \
+                + #propertyName + Urho3D::String (" (Array <") + #asArrayValuesType \
+                + Urho3D::String (">@ array)")).CString (), \
+                asFUNCTION (cxxObjectType ## _set_ ## propertyName), \
                 asCALL_CDECL_OBJFIRST) \
-                ); \
-    CHECK_ANGELSCRIPT_RETURN ( \
-                asEngine->RegisterObjectMethod (#asObjectType, (Urho3D::String ("int set_array_") \
-                + #propertyName + Urho3D::String ("_value (int index, ") + #asArrayValuesType \
-                + Urho3D::String (" value)")).CString (), \
-                asFUNCTION (cxxObjectType ## _set_value_of_ ## propertyName ## _at_index), \
-                asCALL_CDECL_OBJFIRST) \
-                )
+                );
