19580049e4a8d45b972840774815955ab7574a0f
Konstant <ekit.mail@gmail.com>
2016-12-30 22:48:41 +0200

    Fix TestMapPathFinding.

--- sources/Tests/TestMapPathFinding.cpp
+++ sources/Tests/TestMapPathFinding.cpp
@@ -2,6 +2,7 @@
 #include <Urho3D/Input/Input.h>
 #include <Urho3D/Core/CoreEvents.h>
 
+#include <Urho3D/Scene/Scene.h>
 #include <Urho3D/IO/Log.h>
 #include <Colonization/Core/District.hpp>
 #include <Colonization/Core/Map.hpp>
@@ -44,24 +45,34 @@ void TestMapPathFindingApplication::Start ()
     context_->SetGlobalVar ("embarkationSpeed", 0.15f);
     context_->SetGlobalVar ("disembarkationSpeed", 0.2f);
 
-    Urho3D::SharedPtr <Colonization::Map> map (new Colonization::Map (context_));
+    Urho3D::SharedPtr <Urho3D::Scene> scene (new Urho3D::Scene (context_));
+    Colonization::Map *map = scene->CreateChild ("map")->CreateComponent <Colonization::Map> ();
     const int mapWidth = 5;
     const int mapHeight = 5;
 
     for (int x = 0; x < mapWidth; x++)
         for (int y = 0; y < mapHeight; y++)
         {
-            Colonization::District *district = new Colonization::District (context_);
-            district->name_ = "(" + Urho3D::String (x) + "; " + Urho3D::String (y) + ")";
+            Colonization::District *district = map->CreateDistrict (
+                        "(" + Urho3D::String (x) + "; " + Urho3D::String (y) + ")");
 
-            district->polygonPoints_.Push (Urho3D::Vector3 (x - 0.5f, 0.0f, y - 0.5f));
-            district->polygonPoints_.Push (Urho3D::Vector3 (x + 0.5f, 0.0f, y - 0.5f));
-            district->polygonPoints_.Push (Urho3D::Vector3 (x + 0.5f, 0.0f, y + 0.5f));
-            district->polygonPoints_.Push (Urho3D::Vector3 (x - 0.5f, 0.0f, y + 0.5f));
+            Urho3D::PODVector <Urho3D::Vector3> polygonPoints;
+            polygonPoints.Push (Urho3D::Vector3 (x, 0.0f, y ));
+            polygonPoints.Push (Urho3D::Vector3 (x + 1.0f, 0.0f, y));
+            polygonPoints.Push (Urho3D::Vector3 (x + 1.0f, 0.0f, y + 1.0f));
+            polygonPoints.Push (Urho3D::Vector3 (x, 0.0f, y + 1.0f));
+            district->SetPolygonPoints (polygonPoints);
 
-            district->unitPosition_ = Urho3D::Vector3 (x, 0.0f, y);
-            district->colonyPosition_ = Urho3D::Vector3 (x, 0.0f, y);
-            map->CreateDistrict (district);
+            district->SetClimate (Colonization::CLIMATE_TEMPERATE);
+            district->SetFarmingSquare (250.0f);
+            district->SetLandAverageFertility (1.0f);
+            district->SetForestsSquare (150.0f);
+            district->SetForestsReproductivity (0.75f);
+
+            district->SetUnitPosition (Urho3D::Vector3 (x + 0.5f, 0.0f, y + 0.65f));
+            district->SetColonyPosition (Urho3D::Vector3 (x + 0.5f, 0.0f, y + 0.2f));
+            district->SetColonyOwnerName ("PlayerX");
+            district->UpdateHash (map);
         }
 
     // Map: (~ -- sea, = -- terrain, @ -- colony)
@@ -75,65 +86,61 @@ void TestMapPathFindingApplication::Start ()
     // End point: (2, 2).
 
     // array (X * HEIGHT + Y) = (X, Y)
-    map->GetDistrictByIndex (1 * mapHeight + 3)->isSea_ = false;
-    map->GetDistrictByIndex (2 * mapHeight + 3)->isSea_ = false;
-    map->GetDistrictByIndex (3 * mapHeight + 3)->isSea_ = false;
-    map->GetDistrictByIndex (3 * mapHeight + 2)->isSea_ = false;
-    map->GetDistrictByIndex (3 * mapHeight + 1)->isSea_ = false;
-    map->GetDistrictByIndex (2 * mapHeight + 1)->isSea_ = false;
-    map->GetDistrictByIndex (1 * mapHeight + 1)->isSea_ = false;
-    map->GetDistrictByIndex (1 * mapHeight + 0)->isSea_ = false;
+    map->GetDistrictByIndex (1 * mapHeight + 3)->SetIsSea (false);
+    map->GetDistrictByIndex (2 * mapHeight + 3)->SetIsSea (false);
+    map->GetDistrictByIndex (3 * mapHeight + 3)->SetIsSea (false);
+    map->GetDistrictByIndex (3 * mapHeight + 2)->SetIsSea (false);
+    map->GetDistrictByIndex (3 * mapHeight + 1)->SetIsSea (false);
+    map->GetDistrictByIndex (2 * mapHeight + 1)->SetIsSea (false);
+    map->GetDistrictByIndex (1 * mapHeight + 1)->SetIsSea (false);
+    map->GetDistrictByIndex (1 * mapHeight + 0)->SetIsSea (false);
 
-    map->GetDistrictByIndex (2 * mapHeight + 1)->hasColony_ = true;
-    map->GetDistrictByIndex (2 * mapHeight + 1)->colonyOwnerName_ = "PlayerX";
+    map->GetDistrictByIndex (2 * mapHeight + 1)->SetColony (true);
+    map->GetDistrictByIndex (2 * mapHeight + 1)->SetColonyOwnerName ("PlayerX");
     map->RecalculateDistrictsNeighbors ();
 
-    // Test map's serialization.
-    Urho3D::Node *node = new Urho3D::Node (context_);
-    map->UpdateDataNode (node, true);
-    map->ReadDataFromNode (node);
-
     // Firstly calculate path if ship owner isn't colony owner.
-    Urho3D::PODVector <Colonization::District *> way = map->FindPath (
-                map->GetDistrictByIndex (4 * mapHeight + 0), map->GetDistrictByIndex (2 * mapHeight + 2), "PlayerX", true, false);
+    Urho3D::PODVector <Urho3D::StringHash> way = map->FindPath (
+                map->GetDistrictByIndex (4 * mapHeight + 0)->GetHash (),
+                map->GetDistrictByIndex (2 * mapHeight + 2)->GetHash (), "PlayerX", true, false);
 
     Urho3D::String calculatedWay = "";
     for (int index = 0; index < way.Size (); index++)
-        calculatedWay += way.At (index)->name_ + "\n";
+        calculatedWay += map->GetDistrictByHash (way.At (index))->GetName () + "\n";
 
     Urho3D::Log::Write (Urho3D::LOG_INFO, "First calculated way:\n" + calculatedWay);
     if (way.Size () != 5)
         ErrorExit ("First way points size don't match expected.");
-    else if (way.At (0) != map->GetDistrictByIndex (4 * mapHeight + 0) ||
-             way.At (1) != map->GetDistrictByIndex (3 * mapHeight + 0) ||
-             way.At (2) != map->GetDistrictByIndex (2 * mapHeight + 0) ||
-             way.At (3) != map->GetDistrictByIndex (2 * mapHeight + 1) ||
-             way.At (4) != map->GetDistrictByIndex (2 * mapHeight + 2))
+    else if (way.At (0) != map->GetDistrictByIndex (4 * mapHeight + 0)->GetHash ()||
+             way.At (1) != map->GetDistrictByIndex (3 * mapHeight + 0)->GetHash () ||
+             way.At (2) != map->GetDistrictByIndex (2 * mapHeight + 0)->GetHash () ||
+             way.At (3) != map->GetDistrictByIndex (2 * mapHeight + 1)->GetHash () ||
+             way.At (4) != map->GetDistrictByIndex (2 * mapHeight + 2)->GetHash ())
         ErrorExit ("First way don't match expected.");
     else
     {
-        way = map->FindPath (map->GetDistrictByIndex (4 * mapHeight + 0), map->GetDistrictByIndex (2 * mapHeight + 2), "PlayerY", true, false);
+        way = map->FindPath (map->GetDistrictByIndex (4 * mapHeight + 0)->GetHash (),
+                             map->GetDistrictByIndex (2 * mapHeight + 2)->GetHash (), "PlayerY", true, false);
         calculatedWay = "";
         for (int index = 0; index < way.Size (); index++)
-            calculatedWay += way.At (index)->name_ + "\n";
+            calculatedWay += map->GetDistrictByHash (way.At (index))->GetName () + "\n";
 
         Urho3D::Log::Write (Urho3D::LOG_INFO, "Second calculated way:\n" + calculatedWay);
-
         if (way.Size () != 13)
             ErrorExit ("Second way points size don't match expected.");
-        else if (way.At (0) != map->GetDistrictByIndex (4 * mapHeight + 0) ||
-                 way.At (1) != map->GetDistrictByIndex (4 * mapHeight + 1) ||
-                 way.At (2) != map->GetDistrictByIndex (4 * mapHeight + 2) ||
-                 way.At (3) != map->GetDistrictByIndex (4 * mapHeight + 3) ||
-                 way.At (4) != map->GetDistrictByIndex (4 * mapHeight + 4) ||
-                 way.At (5) != map->GetDistrictByIndex (3 * mapHeight + 4) ||
-                 way.At (6) != map->GetDistrictByIndex (2 * mapHeight + 4) ||
-                 way.At (7) != map->GetDistrictByIndex (1 * mapHeight + 4) ||
-                 way.At (8) != map->GetDistrictByIndex (0 * mapHeight + 4) ||
-                 way.At (9) != map->GetDistrictByIndex (0 * mapHeight + 3) ||
-                 way.At (10) != map->GetDistrictByIndex (0 * mapHeight + 2) ||
-                 way.At (11) != map->GetDistrictByIndex (1 * mapHeight + 2) ||
-                 way.At (12) != map->GetDistrictByIndex (2 * mapHeight + 2))
+        else if (way.At (0) != map->GetDistrictByIndex (4 * mapHeight + 0)->GetHash () ||
+                 way.At (1) != map->GetDistrictByIndex (4 * mapHeight + 1)->GetHash () ||
+                 way.At (2) != map->GetDistrictByIndex (4 * mapHeight + 2)->GetHash () ||
+                 way.At (3) != map->GetDistrictByIndex (4 * mapHeight + 3)->GetHash () ||
+                 way.At (4) != map->GetDistrictByIndex (4 * mapHeight + 4)->GetHash () ||
+                 way.At (5) != map->GetDistrictByIndex (3 * mapHeight + 4)->GetHash () ||
+                 way.At (6) != map->GetDistrictByIndex (2 * mapHeight + 4)->GetHash () ||
+                 way.At (7) != map->GetDistrictByIndex (1 * mapHeight + 4)->GetHash () ||
+                 way.At (8) != map->GetDistrictByIndex (0 * mapHeight + 4)->GetHash () ||
+                 way.At (9) != map->GetDistrictByIndex (0 * mapHeight + 3)->GetHash () ||
+                 way.At (10) != map->GetDistrictByIndex (0 * mapHeight + 2)->GetHash () ||
+                 way.At (11) != map->GetDistrictByIndex (1 * mapHeight + 2)->GetHash () ||
+                 way.At (12) != map->GetDistrictByIndex (2 * mapHeight + 2)->GetHash ())
             ErrorExit ("Second way don't match expected.");
         else
             engine_->Exit ();
