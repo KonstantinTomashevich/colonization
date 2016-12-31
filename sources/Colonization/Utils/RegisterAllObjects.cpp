6c8aa2ec13a803708d726953da072c8af59d1c52
Konstant <ekit.mail@gmail.com>
2016-12-31 14:44:22 +0200

    Add RegisterAllObjects and BindAll functions.

---
+++ sources/Colonization/Utils/RegisterAllObjects.cpp
@@ -0,0 +1,33 @@
+#include <Colonization/BuildConfiguration.hpp>
+#include "RegisterAllObjects.hpp"
+
+#include <Colonization/Backend/ColoniesManager.hpp>
+#include <Colonization/Backend/MessagesHandler.hpp>
+#include <Colonization/Backend/Player.hpp>
+#include <Colonization/Backend/PlayersManager.hpp>
+#include <Colonization/Backend/TradeProcessor.hpp>
+#include <Colonization/Backend/UnitsManager.hpp>
+
+#include <Colonization/Core/District.hpp>
+#include <Colonization/Core/InternalTradeArea.hpp>
+#include <Colonization/Core/Map.hpp>
+#include <Colonization/Core/PlayerInfo.hpp>
+#include <Colonization/Core/Unit.hpp>
+
+namespace Colonization
+{
+void RegisterAllObjects (Urho3D::Context *context)
+{
+    ColoniesManager::RegisterObject (context);
+    MessagesHandler::RegisterObject (context);
+    PlayersManager::RegisterObject (context);
+    TradeProcessor::RegisterObject (context);
+    UnitsManager::RegisterObject (context);
+
+    District::RegisterObject (context);
+    InternalTradeArea::RegisterObject (context);
+    Map::RegisterObject (context);
+    PlayerInfo::RegisterObject (context);
+    Unit::RegisterObject (context);
+}
+}
