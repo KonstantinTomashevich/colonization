2daf7480ab027197c4c201002c8d472682568ae0
Konstant <ekit.mail@gmail.com>
2016-12-31 14:59:43 +0200

    Update activities.

--- sources/Colonization/Utils/Activity.hpp
+++ sources/Colonization/Utils/Activity.hpp
@@ -3,17 +3,18 @@
 
 namespace Colonization
 {
+class ActivitiesApplication;
 class Activity : public Urho3D::Object
 {
 URHO3D_OBJECT (Activity, Object)
 protected:
-    void *application_;
+    ActivitiesApplication *application_;
 public:
     Activity (Urho3D::Context *context);
     virtual ~Activity ();
 
-    void *GetApplication ();
-    void SetApplication (void *application);
+    ActivitiesApplication *GetApplication ();
+    void SetApplication (ActivitiesApplication *application);
 
     virtual void Start () = 0;
     virtual void Update (float timeStep) = 0;
