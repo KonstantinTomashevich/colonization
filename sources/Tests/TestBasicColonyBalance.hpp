f5adeba1da17509f87439274644abd14ffe28271
Konstant <ekit.mail@gmail.com>
2017-04-04 19:43:12 +0200

    Add TestBasicColonyBalance and remove TestInternalTradeArea.

---
+++ sources/Tests/TestBasicColonyBalance.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <Urho3D/Engine/Application.h>
+
+namespace Tests
+{
+class TestBasicColonyBalanceApplication : public Urho3D::Application
+{
+URHO3D_OBJECT (TestBasicColonyBalanceApplication, Object)
+public:
+    TestBasicColonyBalanceApplication (Urho3D::Context *context);
+    virtual ~TestBasicColonyBalanceApplication ();
+
+    virtual void Setup ();
+    virtual void Start ();
+    virtual void Stop ();
+};
+}
+
