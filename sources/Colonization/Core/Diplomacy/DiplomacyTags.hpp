ecba256cbb4edef0f5da5a0c2756c3a0fc399757
Konstant <ekit.mail@gmail.com>
2017-05-08 16:53:21 +0200

    Add tags for easier nodes search.

---
+++ sources/Colonization/Core/Diplomacy/DiplomacyTags.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <Urho3D/Container/Str.h>
+
+namespace Colonization
+{
+const Urho3D::String TAG_DIPLOMACY_RELATION ("DiplomacyRelation");
+const Urho3D::String TAG_WAR ("War");
+const Urho3D::String TAG_PEACE ("Peace");
+const Urho3D::String TAG_DIPLOMACY_REQUEST ("DiplomacyRequest");
+}
