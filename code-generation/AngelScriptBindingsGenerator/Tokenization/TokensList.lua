b2c813810fa4aff9510d2c618a896f188d856c5a
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-05-24 13:43:05 +0200

    Add auto end of lines skipping to TokensList.

--- code-generation/AngelScriptBindingsGenerator/Tokenization/TokensList.lua
+++ code-generation/AngelScriptBindingsGenerator/Tokenization/TokensList.lua
@@ -2,14 +2,22 @@ if CreateNewClass == nil then
     CreateNewClass = require (scriptDirectory .. "BindingTypes/CreateNewClass")
 end
 
+if Tokens == nil then
+    Tokens = require (scriptDirectory .. "Tokenization/Tokens")
+end
+
 TokensList = CreateNewClass ()
 TokensList.Construct = function (self, tokens)
     self.currentIndex = 1
     self.tokens = tokens
+    self.skipEndOfLineTokens = false
 end
 
 TokensList.PreviousToken = function (self)
     self.currentIndex = self.currentIndex - 1
+    while self.skipEndOfLineTokens and self:CurrentToken () ~= nil and self:CurrentToken ().type == Tokens.EndOfLine do
+        self.currentIndex = self.currentIndex - 1
+    end
     return self:CurrentToken ()
 end
 
@@ -17,8 +25,19 @@ TokensList.CurrentToken = function (self)
     return self.tokens [self.currentIndex]
 end
 
+TokensList.CurrentOrNextToken = function (self)
+    if self.skipEndOfLineTokens and self:CurrentToken ().type == Tokens.EndOfLine then
+        return self:NextToken ()
+    else
+        return self:CurrentToken ()
+    end
+end
+
 TokensList.NextToken = function (self)
     self.currentIndex = self.currentIndex + 1
+    while self.skipEndOfLineTokens and self:CurrentToken () ~= nil and self:CurrentToken ().type == Tokens.EndOfLine do
+        self.currentIndex = self.currentIndex + 1
+    end
     return self:CurrentToken ()
 end
 return TokensList
