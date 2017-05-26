5469da0f9b11dc6f2fdfcf130336ad84a960289c
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-05-26 13:42:13 +0200

    Implement Enum arguments.

--- code-generation/AngelScriptBindingsGenerator/Bindables/Enum.lua
+++ code-generation/AngelScriptBindingsGenerator/Bindables/Enum.lua
@@ -15,22 +15,60 @@ Enum.Construct = function (self, fileName, bindingAguments)
     self.fileName = fileName
     self.name = ""
     self.values = {}
+    self.bindingName = ""
+    self.bindingValues = {}
     self.arguments = bindingAguments
 end
 
 -- Return true if no errors.
 Enum.Parse = function (self, tokensList)
     tokensList.skipEndOfLineTokens = true
-    return (self:SkipUntilEnumKeyword (tokensList) and self:ReadName (tokensList) and self:ReadValues (tokensList))
+    return (self:SkipUntilEnumKeyword (tokensList) and self:ReadName (tokensList) and
+        self:ReadValues (tokensList) and self:ApplyArguments ())
 end
 
 Enum.ToString = function (self, indent)
-    local string = indent .. self.name .. " from file " .. self.fileName .. "\n" .. indent .. "{\n"
+    local string = indent .. self.bindingName
+    if self.bindingName ~= self.name then
+        string = string .. " (from " .. self.name .. ")"
+    end
+    string = string .. " from file " .. self.fileName .. "\n" .. indent .. "{\n"
+
     for index, value in pairs (self.values) do
-        string = string .. indent .. "    " .. value .. "\n"
+        string = string .. indent .. "    "
+        if self.bindingValues [value] == value then
+            string = string .. value
+        elseif self.bindingValues [value] == nil then
+            string = string .. "[exluded " .. value .. "]"
+        else
+            string = string .. self.bindingValues [value] .. " (from " .. value .. ")"
+        end
+        string = string .. "\n"
     end
     string = string .. indent .. "}\n"
     return string
+end
+
+Enum.ApplyArguments = function (self)
+    self.bindingName = self.name
+    for index, value in ipairs (self.values) do
+        self.bindingValues [value] = value
+    end
+
+    for key, value in pairs (self.arguments) do
+        if key == "OverrideName" then
+            self.bindingName = value
+
+        elseif key:find ("Exclude_") == 1 then
+            local toExclude = key:sub (("Exclude_"):len () + 1, key:len ())
+            self.bindingValues [toExclude] = nil
+
+        elseif key:find ("Rename_") == 1 then
+            local toRename = key:sub (("Rename_"):len () + 1, key:len ())
+            self.bindingValues [toRename] = value
+        end
+    end
+    return true
 end
 
 Enum.SkipUntilEnumKeyword = function (self, tokensList)
