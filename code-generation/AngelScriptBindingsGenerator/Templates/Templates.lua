bc0f0c8097c8c850dfbae37bd86eee3bb6866ab7
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-05-30 16:16:24 +0200

    Templates: add indent.

--- code-generation/AngelScriptBindingsGenerator/Templates/Templates.lua
+++ code-generation/AngelScriptBindingsGenerator/Templates/Templates.lua
@@ -1,4 +1,12 @@
 Templates = {}
+Templates.IncludeRelativePath =
+[[#include "${file}"
+]]
+
+Templates.IncludeFullPath =
+[[#include <${file}>
+]]
+
 Templates.ConstantWrapper =
 [[${type} wrapper_${name} ()
 {
@@ -29,47 +37,55 @@ Templates.ClassRegisterFunction =
 [[template <class T> void Register${name} (asIScriptEngine *engine, char *className)
 ]]
 
+Templates.CallStandartRegister =
+[[    Register${name} (engine);
+]]
+
+Templates.CallClassRegister =
+[[    Register${shortName} <${fullName}> (engine, "${bindingName}");
+]]
+
 Templates.DeclareClass =
-[[engine->RegisterObjectType (${name}, ${size}, ${objectType});
+[[    engine->RegisterObjectType ("${name}", ${size}, ${objectType});
 ]]
 
 Templates.RegisterSubclass =
-[[Urho3D::RegisterSubclass <${baseName}, ${inheritorName}> (engine, "${baseBindingName}", "${inheritorBindingName}");
+[[    Urho3D::RegisterSubclass <${baseName}, ${inheritorName}> (engine, "${baseBindingName}", "${inheritorBindingName}");
 ]]
 
 Templates.RegisterClassBase =
-[[Register${baseName} <T> (engine, className);
+[[    Register${baseName} <T> (engine, className);
 ]]
 
 Templates.RegisterConstant =
-[[engine->RegisterGlobalFunction ("${bindingType} get_${bindingName} ()", asFUNCTION (wrapper_${name}), asCALL_CDECL);
+[[    engine->RegisterGlobalFunction ("${bindingType} get_${bindingName} ()", asFUNCTION (wrapper_${name}), asCALL_CDECL);
 ]]
 
 Templates.RegisterEnumType =
-[[engine->RegisterEnum ("${bindingName}");
+[[    engine->RegisterEnum ("${bindingName}");
 ]]
 
 Templates.RegisterEnumValue =
-[[engine->RegisterEnumValue ("${bindingEnumName}", "${bindingValue}", ${value});
+[[    engine->RegisterEnumValue ("${bindingEnumName}", "${bindingValue}", ${value});
 ]]
 
 Templates.RegisterFreeFunctionWithoutArrays =
-[[engine->RegisterGlobalFunction ("${bindingReturnType} ${bindingName} (${bindingArgs})", asFUNCTION (${name}), asCALL_CDECL);
+[[    engine->RegisterGlobalFunction ("${bindingReturnType} ${bindingName} (${bindingArgs})", asFUNCTION (${name}), asCALL_CDECL);
 ]]
 
 Templates.RegisterFreeFunctionWithArrays =
-[[engine->RegisterGlobalFunction ("${bindingReturnType} ${bindingName} (${bindingArgs})", asFUNCTION (wrapper_${name}), asCALL_CDECL);
+[[    engine->RegisterGlobalFunction ("${bindingReturnType} ${bindingName} (${bindingArgs})", asFUNCTION (wrapper_${name}), asCALL_CDECL);
 ]]
 
 Templates.RegisterClassMethodWithoutArrays =
-[[engine->RegisterObjectMethod (className, "${bindingReturnType} ${bindingName} (${bindingArgs}) ${modifers}", asMETHOD (T, ${name}), asCALL_THISCALL);
+[[    engine->RegisterObjectMethod (className, "${bindingReturnType} ${bindingName} (${bindingArgs}) ${modifers}", asMETHOD (T, ${name}), asCALL_THISCALL);
 ]]
 
 Templates.RegisterClassMethodWithArrays =
-[[engine->RegisterObjectMethod (className, "${bindingReturnType} ${bindingName} (${bindingArgs}) ${modifers}", asFUNCTION (wrapper_${ownerClassName}_${name}), asCALL_CDECL_OBJFIRST);
+[[    engine->RegisterObjectMethod (className, "${bindingReturnType} ${bindingName} (${bindingArgs}) ${modifers}", asFUNCTION (wrapper_${ownerClassName}_${name}), asCALL_CDECL_OBJFIRST);
 ]]
 
 Templates.RegisterSubsystem =
-[[engine->RegisterGlobalFunction ("${bindingType} @+ get_${bindingName} ()", asFUNCTION (wrapper_${bindingName}), asCALL_CDECL);
+[[    engine->RegisterGlobalFunction ("${bindingType} @+ get_${bindingName} ()", asFUNCTION (wrapper_${bindingName}), asCALL_CDECL);
 ]]
 return Templates
