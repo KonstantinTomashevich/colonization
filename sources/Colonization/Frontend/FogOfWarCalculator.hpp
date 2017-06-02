059b99af32ba0126e3b1ed279ac237012d64e616
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-06-02 15:56:22 +0200

    Add ASBindGen comments to Frontend.

--- sources/Colonization/Frontend/FogOfWarCalculator.hpp
+++ sources/Colonization/Frontend/FogOfWarCalculator.hpp
@@ -4,6 +4,7 @@
 
 namespace Colonization
 {
+//@ASBindGen Class ObjectType=Ref
 class FogOfWarCalculator : public Urho3D::Component
 {
 URHO3D_OBJECT (FogOfWarCalculator, Component)
@@ -21,9 +22,12 @@ public:
     static void RegisterObject (Urho3D::Context *context);
     void Update (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData);
 
+    //@ASBindGen Function OverrideName=get_playerName
     Urho3D::String GetPlayerName () const;
+    //@ASBindGen Function OverrideName=set_playerName
     void SetPlayerName (const Urho3D::String &playerName);
 
+    //@ASBindGen Function
     bool IsDistrictVisible (Urho3D::StringHash districtHash);
     Urho3D::HashMap <Urho3D::StringHash, bool> GetFogOfWarMap ();
 };
