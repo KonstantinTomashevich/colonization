c4a3aadd2e47f302cdb5ecaacf3e6410f04b0f97
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-06-05 17:36:28 +0200

    NetworkUpdateCount: rename object_ to trackingObject_.

--- sources/Colonization/Utils/Network/NetworkUpdateCounter.hpp
+++ sources/Colonization/Utils/Network/NetworkUpdateCounter.hpp
@@ -9,7 +9,7 @@ class NetworkUpdateCounter : public Urho3D::Component
 {
 URHO3D_OBJECT (NetworkUpdateCounter, Component)
 protected:
-    Urho3D::Serializable *object_;
+    Urho3D::Serializable *trackingObject_;
     /// When more than 100.0f points accumulated, MarkNetworkUpdate will be called.
     float accumulatedUpdatePoints_;
 
@@ -18,10 +18,10 @@ public:
     virtual ~NetworkUpdateCounter ();
     static void RegisterObject (Urho3D::Context *context);
 
-    //@ASBindGen Function AddRef_arg-1 OverrideName=get_object
-    Urho3D::Serializable *GetObject ();
-    //@ASBindGen Function AddRef_arg0 OverrideName=set_object
-    void SetObject (Urho3D::Serializable *object);
+    //@ASBindGen Function AddRef_arg-1 OverrideName=get_trackingObject
+    Urho3D::Serializable *GetTrackingObject ();
+    //@ASBindGen Function AddRef_arg0 OverrideName=set_trackingObject
+    void SetTrackingObject (Urho3D::Serializable *trackingObject);
 
     //@ASBindGen Function OverrideName=get_accumulatedUpdatePoints
     float GetAccumulatedUpdatePoints ();
