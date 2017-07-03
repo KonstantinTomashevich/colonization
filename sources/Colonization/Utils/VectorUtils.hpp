e312d925a6dc030c1b74c9178c0c209718a01750
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-07-03 14:21:17 +0200

    VectorUtils: fix codestyle errors with braces.

--- sources/Colonization/Utils/VectorUtils.hpp
+++ sources/Colonization/Utils/VectorUtils.hpp
@@ -9,16 +9,24 @@ namespace VectorUtils
 template <class T> int GetElementIndex (Urho3D::PODVector <T> &podVector, T element)
 {
     for (int index = 0; index < podVector.Size (); index++)
+    {
         if (podVector.At (index) == element)
+        {
             return index;
+        }
+    }
     return -1;
 }
 
 template <class T> int GetElementIndex (Urho3D::Vector <T> &vector, T element)
 {
     for (int index = 0; index < vector.Size (); index++)
+    {
         if (vector.At (index) == element)
+        {
             return index;
+        }
+    }
     return -1;
 }
 }
